4257669ef565652871652c28e21408d324320273
renatoconstanciofilho <renatoconstanciofilho@gmail.com>
2023-08-15 17:19:07 -0300

    alterado cadastro de contato e visualização dos contatos

--- src/pages/CompanyRegistration.vue
+++ src/pages/CompanyRegistration.vue
@@ -5,10 +5,10 @@
         <div class="row q-col-gutter-sm">
           <div
             class="col"
-            style="max-width: 200px;"
+            style="max-width: 200px"
           >
             <q-input
-              v-model="cnpjNumber"
+              v-model="company.cnpj"
               buttom-slots
               maxlength="18"
               mask="##.###.###/####-##"
@@ -16,13 +16,13 @@
               label="CNPJ"
               :dense="dense"
               error-message="CNPJ inválido"
-              :error="cnpjNumber.length >= 14 && !cnpjIsValid"
-              @blur="() => getCompany(cnpjNumber)"
+              :error="company.cnpj.length >= 14 && !cnpjIsValid"
+              @blur="() => getCompany()"
             />
           </div>
           <div class="col">
             <q-input
-              v-model="companyName"
+              v-model="company.name"
               label="Razão Social"
               :dense="dense"
             />
@@ -31,41 +31,186 @@
         <div class="row q-col-gutter-sm">
           <div class="col-5">
             <q-input
-              v-model="companyAddress"
+              v-model="company.address.name"
               label="Endereço"
               :dense="dense"
             />
           </div>
           <div class="col-1">
             <q-input
-              v-model="companyAddressNumber"
+              v-model="company.address.number"
               label="Número"
               :dense="dense"
             />
           </div>
           <div class="col-2">
             <q-input
-              v-model="companyAddressComplement"
+              v-model="company.address.complement"
               label="Complemento"
               :dense="dense"
             />
           </div>
           <div class="col-3">
             <q-input
-              v-model="companyAddressNeighborhood"
+              v-model="company.address.neighborhood"
               label="Bairro"
               :dense="dense"
             />
           </div>
           <div class="col-1">
             <q-input
-              v-model="companyAddressCep"
+              v-model="company.address.cep"
               label="CEP"
               :dense="dense"
             />
           </div>
         </div>
+        <!-- <q-separator />
+
+        <div class="row q-col-gutter-sm">
+          <div class="col-3">
+            <q-input
+              v-model="newContact.nome"
+              label="nome"
+              :dense="dense"
+            />
+          </div>
+          <div class="col-1">
+            <q-input
+              v-model="newContact.telefone"
+              label="Telefone"
+              :dense="dense"
+            />
+          </div>
+        </div>
+
+        <div class="q-pa-md">
+          <q-table
+            title="Contatos"
+            :rows="company.contacts"
+            :columns="columns"
+            row-key="name"
+          />
+        </div>
+
+        <q-btn
+          color="white"
+          text-color="black"
+          label="Adicionar"
+          @click="adicionarContato"
+        /> -->
       </q-form>
+    </div>
+    <div clas="q-pa-md -q-gutter-sm">
+      <q-btn
+        label="Adicionar Contato"
+        color="primary"
+        @click="addContact = true"
+      />
+      <q-dialog
+        v-model="addContact"
+        persistent
+      >
+        <q-card style="min-width: 500px">
+          <q-card-section>
+            <div class="text-h6">
+              Contato
+            </div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <q-form>
+              <div class="row q-col-gutter-sm">
+                <div class="col-1">
+                  <q-input
+                    v-model="newContact.title"
+                    label="Título"
+                    maxlength="3"
+                    :dense="dense"
+                  />
+                </div>
+                <div class="col-4">
+                  <q-input
+                    v-model="newContact.name"
+                    label="Nome"
+                    :dense="dense"
+                  />
+                </div>
+                <div class="col-7">
+                  <q-input
+                    v-model="newContact.surname"
+                    label="Sobrenome"
+                    :dense="dense"
+                  />
+                </div>
+              </div>
+              <div class="row q-col-gutter-sm">
+                <div class="col-6">
+                  <q-input
+                    v-model="newContact.phone"
+                    label="Telefone"
+                    maxlength="14"
+                    mask="(##) ####-####"
+                    :dense="dense"
+                  />
+                </div>
+                <div class="col-6">
+                  <q-input
+                    v-model="newContact.mobile"
+                    label="Celular"
+                    maxlength="15"
+                    mask="(##) #####-####"
+                    :dense="dense"
+                  />
+                </div>
+              </div>
+              <div class="row q-col-gutter-sm">
+                <div class="col-12">
+                  <q-input
+                    v-model="newContact.email"
+                    label="E-Mail"
+                    :dense="dense"
+                  />
+                </div>
+              </div>
+              <div class="row q-col-gutter-sm">
+                <div class="col-12">
+                  <q-input
+                    v-model="newContact.department"
+                    label="Departamento"
+                    :dense="dense"
+                  />
+                </div>
+              </div>
+            </q-form>
+          </q-card-section>
+
+          <q-card-actions
+            align="right"
+            class="text-primary"
+          >
+            <q-btn
+              v-close-popup
+              flat
+              icon="mdi-close"
+            />
+            <q-btn
+              v-close-popup
+              flat
+              icon="mdi-content-save"
+              @click="newContact"
+            />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+    </div>
+    <div clas="q-pa-md -q-gutter-sm">
+      <q-table
+        title="Contatos"
+        :rows="company.contacts"
+        :columns="columns"
+        row-key="name"
+      />
     </div>
   </q-page>
 </template>
@@ -76,51 +221,148 @@ import { cnpj } from 'cpf-cnpj-validator'
 import { api } from 'src/boot/axios'
 import { useQuasar } from 'quasar'
 
-export default {
+// const columns = [
+//   {
+//     name: 'nome',
+//     align: 'center',
+//     label: 'nome',
+//     field: 'nome',
+//     sortable: true
+//   },
+//   { name: 'telefone', label: 'telefone', field: 'telefone', sortable: true }
+// ]
 
+export default {
   setup () {
-    const cnpjNumber = ref('')
-    const companyName = ref('')
-    const companyAddress = ref('')
-    const companyAddressNumber = ref('')
-    const companyAddressNeighborhood = ref('')
-    const companyAddressComplement = ref('')
-    const companyAddressCep = ref('')
     const $q = useQuasar()
+    const company = ref({
+      cnpj,
+      number: ref(''),
+      name: ref(''),
+      address: ref({
+        name: ref(''),
+        number: ref(''),
+        neighborhood: ref(''),
+        complement: ref(''),
+        cep: ref('')
+      }),
+      contacts: ref([])
+    })
+    const columns = [
+      {
+        name: 'title',
+        required: true,
+        label: 'Título',
+        align: 'left',
+        field: row => company.value.contacts.title,
+        formmat: val => `${val}`,
+        sortable: true
+      },
+      {
+        name: 'name',
+        required: true,
+        label: 'Nome',
+        align: 'left',
+        field: row => company.value.contacts.name,
+        formmat: val => `${val}`,
+        sortable: true
+      },
+      {
+        name: 'surname',
+        required: true,
+        label: 'Sobrenome',
+        align: 'left',
+        field: row => company.value.contacts.surname,
+        formmat: val => `${val}`,
+        sortable: true
+      },
+      {
+        name: 'phone',
+        required: true,
+        label: 'Telefone',
+        align: 'left',
+        field: row => company.value.contacts.phone,
+        formmat: val => `${val}`,
+        sortable: true
+      },
+      {
+        name: 'mobile',
+        required: true,
+        label: 'Celular',
+        align: 'left',
+        field: row => company.value.contacts.mobile,
+        formmat: val => `${val}`,
+        sortable: true
+      },
+      {
+        name: 'email',
+        required: true,
+        label: 'E-Mail',
+        align: 'left',
+        field: row => company.value.contacts.email,
+        formmat: val => `${val}`,
+        sortable: true
+      },
+      {
+        name: 'department',
+        required: true,
+        label: 'Departamento',
+        align: 'left',
+        field: row => company.value.contacts.department,
+        formmat: val => `${val}`,
+        sortable: true
+      }
+    ]
     return {
-      cnpjNumber,
-      companyName,
-      companyAddress,
-      companyAddressNumber,
-      companyAddressNeighborhood,
-      companyAddressComplement,
-      companyAddressCep,
+      addContact: ref(false),
+      company,
+      columns,
+      newContact: ref({}),
       $q,
-      cnpjIsValid: computed(() => cnpj.isValid(cnpjNumber.value))
+      cnpjIsValid: computed(() => {
+        return cnpj.isValid(company.value.cnpj)
+      })
     }
   },
   // name: 'PageName',
 
   methods: {
-    getCompany (data) {
-      // console.log(data)
-      api.get('https://minhareceita.org/' + data).then((response) => {
-        this.companyName = response.data.razao_social
-        this.companyAddress = response.data.descricao_tipo_de_logradouro + ' ' + response.data.logradouro
-        this.companyAddressNumber = response.data.numero
-        this.companyAddressNeighborhood = response.data.bairro
-        this.companyAddressComplement = response.data.complemento
-        this.companyAddressCep = response.data.cep
-      }).catch(() => {
-        this.$q.notify({
-          color: 'negative',
-          position: 'top',
-          message: 'Erro ao consultar CNPJ',
-          icon: 'mdi-alert'
-        })
+    adicionarContato () {
+      this.company.contacts.push({
+        title: this.newContact.title,
+        name: this.newContact.name,
+        surname: this.newContact.surname,
+        department: this.newContact.department,
+        phone: this.newContact.phone,
+        mobile: this.newContact.mobile,
+        email: this.newContact.email
       })
+      console.log(this.company.contacts[0])
+    },
+    getCompany () {
+      // console.log(data)
+      api
+        .get('https://minhareceita.org/' + this.company.cnpj)
+        .then((response) => {
+          this.company.name = response.data.razao_social
+          this.company.address.name =
+            response.data.descricao_tipo_de_logradouro +
+            ' ' +
+            response.data.logradouro
+          this.company.address.number = response.data.numero
+          this.company.address.neighborhood = response.data.bairro
+          this.company.address.complement = response.data.complemento
+          this.company.address.cep = response.data.cep
+        })
+        .catch(() => {
+          this.$q.notify({
+            color: 'negative',
+            position: 'top',
+            message: 'Erro ao consultar CNPJ',
+            icon: 'mdi-alert'
+          })
+        })
     }
   }
 }
-
 </script>
